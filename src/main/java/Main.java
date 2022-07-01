aa835d5cc6369bded56ce0098e95f8b104a4f53b
Jamie Scofield <jamies_1048@outlook.com>
2022-07-01 16:20:20 +0100

    Added merge method to merge two arrays

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,30 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
 
     public static void main(String[] args) {
+        boolean carryon = true;
+        ArrayList<Integer> input = new ArrayList();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome, please input the numbers you want");
+        while(carryon){
+            System.out.println("please input value");
+            int n = scanner.nextInt();
+            input.add(n);
+            System.out.println("do you want to add another value (y/n)");
+            String x = scanner.next();
+            if(x.equals("n")){
+                carryon = false;
+            }
+        }
+        int [] inputarray = new int[input.size()];
+        for(int i = 0; i < input.size();i++){
+            inputarray[i] = input.get(i);
+        }
+
+        System.out.println("The inputted array "+ input);
 
     }
-
-
-
 }
---
+++ src/main/java/Merger.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Merger {
+
+    public int[] merge(int[] arrayA, int[] arrayB){
+        ArrayList<Integer> listA = new ArrayList<Integer>();
+        ArrayList<Integer> listB = new ArrayList<Integer>();
+        for(int i:arrayA){
+            listA.add(i);
+        }
+        for(int i:arrayB){
+            listB.add(i);
+        }
+        ArrayList<Integer> listTotal = new ArrayList<Integer>();
+        listTotal.addAll(listA);
+        listTotal.addAll(listB);
+        int[] newArray = new int[listTotal.size()];
+        for(int i = 0; i< listTotal.size();i++){
+            newArray[i] = listTotal.get(i);
+        }
+        return newArray;
+    }
+}
